126cba6ef324b5fdd4aa24700e49416d9d255d59
Romario Gustave <romagustave10@gmail.com>
2020-01-07 23:13:03 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,19 @@
 class Appointement 
-  attr_reader
+  attr_reader :patient, :doctor, :date 
+  @@all = []
+  
+  def initialize(patient="", doctor="", date="")
+    @patient = patient
+    @doctor = doctor
+    @date = date 
+    @all << self 
+  end 
+  
+  def self.all 
+    @all 
+  end 
+  
+  def patients 
+    self.patient
+  end 
+  def doctors
