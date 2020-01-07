d132a6462c432f5bbc6595ef67bfc351b97edbae
Romario Gustave <romagustave10@gmail.com>
2020-01-07 23:21:04 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,6 +1,18 @@
 class Genre 
+  attr_reader :name , :genre
+  attr_accessor :song 
   
-  def initialize(genre)
-    @genre = genre 
-    
+  def initialize(name)
+   @name = name 
+ end 
+ def song 
+   song.all.select do |song|
+     song.genre == self 
+   end 
+ end 
+ def artists 
+   song.all.map do |song|
+     song.artist 
+   end 
+ end 
 end 
