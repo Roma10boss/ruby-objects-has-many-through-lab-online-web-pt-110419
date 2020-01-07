f2bc7046a047441232f0dbb4f4c65b1ee399d783
Romario Gustave <romagustave10@gmail.com>
2020-01-07 23:17:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,29 @@
 class  Artist 
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :genre 
   @all = []
   
+  def initialize 
+    @name = name 
+    @@all << self 
+  end 
   
+  def self.all 
+    @@all 
+  end 
   
+  def new_song(name,genre)
+    song = Song.new(name, self, genre)
+    self.songs.last 
+  end 
   
-end 
+  def songs 
+    song.all.select do |song|
+      song.artist == self 
+    end 
+    
+    def genre 
+      song.all.map do |song|
+        song.genre 
+      end 
+    end 
+  end 
