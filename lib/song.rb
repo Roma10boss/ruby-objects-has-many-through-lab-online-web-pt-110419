3c4ce4fe09ecddeb675dfd93191eab1a7a7454dc
Romario Gustave <romagustave10@gmail.com>
2020-01-07 23:08:44 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class  Song 
+  attr_accessor :song :artist :genre 
+  @all = []
+  
+  def new_song 
+    song.artist 
+  end 
+  
+  def genre 
+    @genre
+  end 
+  
+  def artist 
+    @artist
+  end 
+  
+end 
